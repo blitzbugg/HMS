697dad4193dcd5ef772b9e92cc7cf6a1a191f102
Ananthapadmanabhan M <ananthapadmanabhanr2@gmail.com>
2024-09-16 22:51:08 +0530

    Almost created the side nav menu and added the menu boxes for admin

--- admin/index.php
+++ admin/index.php
@@ -13,5 +13,79 @@ session_start();
     <?php
         include("../include/header.php");
     ?>
+    <div class="container-fluid">
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-2" style="margin-left: -30px">
+                    <?php
+                    include("sidenav.php");
+                    ?>
+                </div>
+                <div class="col-md-10">
+                    <h4 class="my-2">Admin Dashboard</h4>
+                    <div class="col-md-12 my-1">
+                        <div class="row">
+                            <div class="col-md-3 bg-success mx-2" style="height: 130px;">
+                                <div class="col-md-12">
+                                    <div class="row">
+                                        <div class="col-md-8">
+                                            <h5 class="my-2 text-white fs-1">0</h5>
+                                            <h5 class="text-white">Total</h5>
+                                            <h5 class="text-white">Admin</h5>
+                                        </div>
+                                        <div class="col-md-3">
+                                            <a href=""><i class="fa-solid fa-user-gear fa-2xl p-5" style="color: #ffffff;"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-3 bg-info mx-2" style="height: 130px;">
+                                <div class="col-md-12">
+                                    <div class="row">
+                                        <div class="col-md-8">
+                                            <h5 class="my-2 text-white fs-1">0</h5>
+                                            <h5 class="text-white">Total</h5>
+                                            <h5 class="text-white">Admin</h5>
+                                        </div>
+                                        <div class="col-md-3">
+                                            <a href=""><i class="fa-solid fa-user-gear fa-2xl p-5" style="color: #ffffff;"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-3 bg-warning mx-2" style="height: 130px;">
+                                <div class="col-md-12">
+                                    <div class="row">
+                                        <!-- Add content here -->   
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-3 bg-danger mx-2 my-2" style="height: 130px;">
+                                <div class="col-md-12">
+                                    <div class="row">
+                                        <!-- Add content here -->
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-3 bg-warning mx-2 my-2" style="height: 130px;">
+                                <div class="col-md-12">
+                                    <div class="row">
+                                        <!-- Add content here -->
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-3 bg-success mx-2 my-2" style="height: 130px;">
+                                <div class="col-md-12">
+                                    <div class="row">
+                                        <!-- Add content here -->
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
---
+++ admin/sidenav.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+</head>
+<body>
+    <!-- Sidenav -->
+
+    <div class="list-group bg-info" style="height: 90vh">
+        <a href="index.php" class="list-group-item list-group-item-action bg-info text-center text-white">Dashboard</a>
+        <a href="" class="list-group-item list-group-item-action bg-info text-center text-white">Administrators</a>
+        <a href="" class="list-group-item list-group-item-action bg-info text-center text-white">Doctors</a>
+        <a href="" class="list-group-item list-group-item-action bg-info text-center text-white">Patients</a>
+     </div>
+     <!-- Ends -->
+</body>
+</html>
