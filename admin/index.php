667bbe7c8f99c9e49022486a35d8edadaea9d236
Ananthapadmanabhan M <ananthapadmanabhanr2@gmail.com>
2024-09-17 10:29:52 +0530

    Admin dashboard connention has established

--- admin/index.php
+++ admin/index.php
@@ -12,6 +12,7 @@ session_start();
 <body>
     <?php
         include("../include/header.php");
+        include("../include/connection.php");
     ?>
     <div class="container-fluid">
         <div class="col-md-12">
@@ -29,7 +30,12 @@ session_start();
                                 <div class="col-md-12">
                                     <div class="row">
                                         <div class="col-md-8">
-                                            <h5 class="my-2 text-white fs-1">0</h5>
+                                            <!-- Php admin query -->
+                                            <?php
+                                                $ad = mysqli_query($connect,"SELECT * FROM admin");
+                                                $num = mysqli_num_rows($ad);
+                                            ?>
+                                            <h5 class="my-2 text-white fs-1"><?php echo $num; ?></h5>
                                             <h5 class="text-white">Total</h5>
                                             <h5 class="text-white">Admin</h5>
                                         </div>
@@ -45,10 +51,10 @@ session_start();
                                         <div class="col-md-8">
                                             <h5 class="my-2 text-white fs-1">0</h5>
                                             <h5 class="text-white">Total</h5>
-                                            <h5 class="text-white">Admin</h5>
+                                            <h5 class="text-white">Doctor</h5>
                                         </div>
                                         <div class="col-md-3">
-                                            <a href=""><i class="fa-solid fa-user-gear fa-2xl p-5" style="color: #ffffff;"></i></a>
+                                            <a href=""><i class="fa-solid fa-user-doctor fa-2xl p-5" style="color: #ffffff;"></i></a>
                                         </div>
                                     </div>
                                 </div>
@@ -56,28 +62,56 @@ session_start();
                             <div class="col-md-3 bg-warning mx-2" style="height: 130px;">
                                 <div class="col-md-12">
                                     <div class="row">
-                                        <!-- Add content here -->   
+                                        <div class="col-md-8">
+                                            <h5 class="my-2 text-white fs-1">0</h5>
+                                            <h5 class="text-white">Total</h5>
+                                            <h5 class="text-white">Patient</h5>
+                                        </div>
+                                        <div class="col-md-3">
+                                            <a href=""><i class="fa-solid fa-bed fa-2xl p-5" style="color: #ffffff;"></i></a>
+                                        </div>  
                                     </div>
                                 </div>
                             </div>
                             <div class="col-md-3 bg-danger mx-2 my-2" style="height: 130px;">
                                 <div class="col-md-12">
                                     <div class="row">
-                                        <!-- Add content here -->
+                                        <div class="col-md-8">
+                                            <h5 class="my-2 text-white fs-1">0</h5>
+                                            <h5 class="text-white">Total</h5>
+                                            <h5 class="text-white">Report</h5>
+                                        </div>
+                                        <div class="col-md-3">
+                                            <a href=""><i class="fa-solid fa-flag fa-2xl p-5" style="color: #ffffff;"></i></a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-md-3 bg-warning mx-2 my-2" style="height: 130px;">
                                 <div class="col-md-12">
                                     <div class="row">
-                                        <!-- Add content here -->
+                                        <div class="col-md-8">
+                                            <h5 class="my-2 text-white fs-1">0</h5>
+                                            <h5 class="text-white">Total</h5>
+                                            <h5 class="text-white">Job Request</h5>
+                                        </div>
+                                        <div class="col-md-3">
+                                            <a href=""><i class="fa-solid fa-book-open-reader fa-2xl p-5" style="color: #ffffff;"></i></a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-md-3 bg-success mx-2 my-2" style="height: 130px;">
                                 <div class="col-md-12">
                                     <div class="row">
-                                        <!-- Add content here -->
+                                        <div class="col-md-8">
+                                            <h5 class="my-2 text-white fs-1">0</h5>
+                                            <h5 class="text-white">Total</h5>
+                                            <h5 class="text-white">Income</h5>
+                                        </div>
+                                        <div class="col-md-3">
+                                            <a href=""><i class="fa-solid fa-indian-rupee-sign fa-2xl p-5" style="color: #ffffff;"></i></a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
