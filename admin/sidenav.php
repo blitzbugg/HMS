25a9ff2255654031cee06e5fb95f71eb52abe912
Ananthapadmanabhan M <ananthapadmanabhanr2@gmail.com>
2024-09-21 12:41:48 +0530

    bg colour has changed

--- admin/sidenav.php
+++ admin/sidenav.php
@@ -8,11 +8,12 @@
 <body>
     <!-- Sidenav -->
 
-    <div class="list-group bg-info" style="height: 90vh; margin-left: -30px;">
-        <a href="index.php" class="list-group-item list-group-item-action bg-info text-center text-white">Dashboard</a>
-        <a href="" class="list-group-item list-group-item-action bg-info text-center text-white">Administrators</a>
-        <a href="" class="list-group-item list-group-item-action bg-info text-center text-white">Doctors</a>
-        <a href="" class="list-group-item list-group-item-action bg-info text-center text-white">Patients</a>
+    <div class="list-group bg-secondary" style="height: 90vh; margin-left: -30px;">
+        <a href="index.php" class="list-group-item list-group-item-action bg-secondary text-center text-white">Dashboard</a>
+        <a href="profile.php" class="list-group-item list-group-item-action bg-secondary text-center text-white">Profile</a>
+        <a href="" class="list-group-item list-group-item-action bg-secondary text-center text-white">Administrators</a>
+        <a href="" class="list-group-item list-group-item-action bg-secondary text-center text-white">Doctors</a>
+        <a href="" class="list-group-item list-group-item-action bg-secondary text-center text-white">Patients</a>
      </div>
      <!-- Ends -->
 </body>
