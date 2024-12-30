b80d87ba1b0c5abf2218a02510c89b429ecea66f
Ananthapadmanabhan M <ananthapadmanabhanr2@gmail.com>
2024-12-30 21:13:33 +0530

    Changed the styles of doctor login page

--- doctorlogin.php
+++ doctorlogin.php
@@ -5,36 +5,27 @@ include("include/connection.php");
 $show = "";
 
 if (isset($_POST['login'])) {
-
     $uname = $_POST['uname'];
     $password = $_POST['pass'];
+    $error = array();
 
-    $error = array(); 
-
-    // Check if username and password are empty
     if (empty($uname)) {
         $error['login'] = "Enter Username";
     } else if (empty($password)) {
         $error['login'] = "Enter Password";
     } else {
-        // Prepare query to check if the username and password match
         $q = "SELECT * FROM doctors WHERE username='$uname' AND password='$password'";
         $qq = mysqli_query($connect, $q);
 
-        // Check if any row is returned
         if (mysqli_num_rows($qq) > 0) {
             $row = mysqli_fetch_array($qq);
-
-            // Check the status of the user
             if ($row['status'] == "pending") {
                 $error['status'] = "Please wait for the admin to confirm";
             } else if ($row['status'] == "Rejected") {
                 $error['login'] = "Try again later";
             } else {
-                // Successful login
                 $_SESSION['doctor'] = $uname;
                 echo "<script>alert('done')</script>";
-                // Redirect to doctor's dashboard or any other page
                 header("Location: doctor/index.php");
             }
         } else {
@@ -42,7 +33,6 @@ if (isset($_POST['login'])) {
         }
     }
 
-    // Display error message
     if (isset($error['login'])) {
         $l = $error['login'];
         $show = "<h5 class='text-center alert alert-danger'>$l</h5>";
@@ -55,37 +45,46 @@ if (isset($_POST['login'])) {
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Doctor Login Page</title>
+    <title>Doctor Login - Healthcare Plus</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="./css/doctorlogin.css">
 </head>
-<body style="background-image: url(img/admin-back.jpg); background-size: cover; background-repeat: no-repeat;">
+<body>
     <?php include("include/header.php"); ?>
-    <div class="container-fluid mt-5">
-        <div class="col-md-12">
-            <div class="row">
-                <div class="col-md-3"></div>
-                <div class="col-md-6 p-5 bg-light mt-5 rounded-4">
-                    <h5 class="text-center my-2">Doctors Login</h5>
-                    <div>
-                        <?php echo $show; ?>
-                    </div>
+    
+    <div class="container">
+        <div class="row min-vh-100 align-items-center">
+            <div class="col-lg-6 col-md-8 mx-auto">
+                <div class="login-card">
+                    <h2 class="login-title">Doctor Login</h2>
+                    <?php echo $show; ?>
+                    
                     <form method="post">
                         <div class="form-group">
                             <label>Username</label>
-                            <input type="text" name="uname" class="form-control" autocomplete="off" placeholder="Enter Username">
+                            <input type="text" name="uname" class="form-control" autocomplete="off" 
+                                   placeholder="Enter your username">
                         </div>
                         
                         <div class="form-group">
                             <label>Password</label>
-                            <input type="password" name="pass" class="form-control" autocomplete="off">
+                            <input type="password" name="pass" class="form-control" autocomplete="off"
+                                   placeholder="Enter your password">
                         </div>
-                        <input type="submit" name="login" class="btn btn-success" value="Login">
+                        
+                        <button type="submit" name="login" class="btn btn-login">
+                            Login to Dashboard
+                        </button>
 
-                        <p>I don't have any account <a href="apply.php">Signup</a></p>
+                        <div class="signup-link">
+                            <p>Don't have an account? <a href="apply.php">Sign up</a></p>
+                        </div>
                     </form>
                 </div>
-                <div class="col-md-3"></div>
             </div>
         </div>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
