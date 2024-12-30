a06a7f3574744803ee215a37f92c42a5c21c954a
Ananthapadmanabhan M <ananthapadmanabhanr2@gmail.com>
2024-12-30 20:37:51 +0530

    Added the sql file which is needed to create all the essential tables

--- sql/hms.sql
+++ sql/hms.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 24, 2024 at 09:40 AM
+-- Generation Time: Dec 30, 2024 at 04:04 PM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,34 @@ SET time_zone = "+00:00";
 --
 -- Database: `hms`
 --
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `GeneratePatientID` (IN `firstName` VARCHAR(100), IN `surName` VARCHAR(100), IN `userName` VARCHAR(100), IN `email` VARCHAR(100), IN `phone` VARCHAR(100), IN `gender` VARCHAR(100), IN `district` VARCHAR(100), IN `passWord` VARCHAR(100), IN `profile` VARCHAR(100))   BEGIN
+    DECLARE regDate VARCHAR(6);
+    DECLARE patientCount INT;
+    DECLARE newPatientID INT;
+
+    -- Get the current date in the format YYMMDD
+    SET regDate = DATE_FORMAT(CURDATE(), '%y%m%d');
+
+    -- Count how many patients registered today
+    SELECT COUNT(*) INTO patientCount 
+    FROM patient 
+    WHERE date_reg = regDate;
+
+    -- Generate the new patient ID: today's date + (count + 1)
+    SET newPatientID = CONCAT(regDate, patientCount + 1);
+
+    -- Insert the new patient with the generated PatientID
+    INSERT INTO patient (id, firstname, surname, username, email, phone, gender, district, password, date_reg, profile)
+    VALUES (newPatientID, firstName, surName, userName, email, phone, gender, district, passWord, regDate, profile);
+
+END$$
+
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -40,6 +68,24 @@ CREATE TABLE `admin` (
 
 INSERT INTO `admin` (`id`, `username`, `password`, `profile`) VALUES
 (15, 'Andrew', 'andrew', 'admin.jpeg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointment`
+--
+
+CREATE TABLE `appointment` (
+  `id` int(100) NOT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `surname` varchar(100) NOT NULL,
+  `gender` varchar(100) NOT NULL,
+  `phone` varchar(100) NOT NULL,
+  `appointment_date` varchar(100) NOT NULL,
+  `symptoms` varchar(100) NOT NULL,
+  `status` varchar(100) NOT NULL,
+  `date_booked` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -71,6 +117,63 @@ INSERT INTO `doctors` (`id`, `firstname`, `surname`, `username`, `email`, `gende
 (1, 'John', 'Mathew', 'jom', 'john@gmail.com', 'Male', '9496095846', 'India', '1234', '2000', '2024-10-22 19:06:20', 'Approved', 'john-doc.jpeg'),
 (2, 'Jins', 'Joseph', 'jin', 'jin@gmail.com', 'Male', '89877865432', 'Russia', 'abcd', '10000', '2024-10-24 12:48:11', 'Approved', 'doctor.jpg');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `income`
+--
+
+CREATE TABLE `income` (
+  `id` int(100) NOT NULL,
+  `doctor` varchar(100) NOT NULL,
+  `patient` varchar(100) NOT NULL,
+  `date_discharge` varchar(100) NOT NULL,
+  `amount_paid` varchar(100) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `date_check` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient`
+--
+
+CREATE TABLE `patient` (
+  `id` int(100) NOT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `surname` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone` varchar(100) NOT NULL,
+  `gender` varchar(100) NOT NULL,
+  `district` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `date_reg` varchar(100) NOT NULL,
+  `profile` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `patient`
+--
+
+INSERT INTO `patient` (`id`, `firstname`, `surname`, `username`, `email`, `phone`, `gender`, `district`, `password`, `date_reg`, `profile`) VALUES
+(2412301, 'manu', 'm', 'manu', 'manu@gmail.com', '09496095846', 'Male', 'Pathanamthitta', '1111', '2024-12-30 19:58:17', 'patient.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `report`
+--
+
+CREATE TABLE `report` (
+  `id` int(100) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `message` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `date_send` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 --
 -- Indexes for dumped tables
 --
@@ -82,9 +185,27 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `appointment`
+--
+ALTER TABLE `appointment`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `doctors`
 --
 ALTER TABLE `doctors`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `income`
+--
+ALTER TABLE `income`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `report`
+--
+ALTER TABLE `report`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -98,10 +219,28 @@ ALTER TABLE `admin`
   MODIFY `id` int(7) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
+-- AUTO_INCREMENT for table `appointment`
+--
+ALTER TABLE `appointment`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `doctors`
 --
 ALTER TABLE `doctors`
   MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `income`
+--
+ALTER TABLE `income`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `report`
+--
+ALTER TABLE `report`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
