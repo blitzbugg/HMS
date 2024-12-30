1d134b53f84c8a0318a33e8d5b002a06c1863597
Ananthapadmanabhan M <ananthapadmanabhanr2@gmail.com>
2024-12-30 21:09:15 +0530

    Changed the style of admin login page

--- adminlogin.php
+++ adminlogin.php
@@ -1,7 +1,5 @@
 <?php
-
 session_start();
-
 include("include/connection.php");
 
 if(isset($_POST['login'])){
@@ -9,7 +7,6 @@ if(isset($_POST['login'])){
     $password = $_POST['password'];
 
     $alert = "";
-    
     $error = array();
 
     if (empty($username)) {
@@ -23,14 +20,11 @@ if(isset($_POST['login'])){
 
     if (count($error)==0) {
         $query = "SELECT * FROM admin WHERE username = '$username' AND password = '$password'";
-
         $result = mysqli_query($connect,$query);
 
         if (mysqli_num_rows($result) == 1) {
             echo "<script>alert('You have loggined as admin')</script>";
-
             $_SESSION['admin'] = $username;
-
             header("Location: admin/index.php");
             exit();
         }
@@ -39,53 +33,55 @@ if(isset($_POST['login'])){
         }
     }
 }
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin</title>
+    <title>Admin Login - Healthcare Plus</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="./css/adminlogin.css">
 </head>
-<body style="background-image: url(img/admin-back.jpg); background-repeat: no-repeat; background-size: cover;">
-    <?php
-        include("include/header.php")
-    ?>
+<body>
+    <?php include("include/header.php") ?>
 
     <div class="container">
-        <div class="col-md-12">
-            <div class="row">
-                <div class="col-md-3"></div>
-                    <div class="col-md-6 p-4 bg-dark-subtle mt-5 rounded-4">
-                    <img src="img/admin-login.jpg" alt="" class="col-md-5 rounded mx-auto d-block">
-                    <form action="" method="post" name="adm-login" class="my-2">
+        <div class="row min-vh-100 align-items-center">
+            <div class="col-lg-6 col-md-8 mx-auto">
+                <div class="login-container">
+                    <img src="img/admin-login.jpg" alt="Admin Login" class="img-fluid mx-auto d-block">
+                    <form action="" method="post" name="adm-login">
+                        <?php if(isset($alert) && !empty($alert)): ?>
+                            <div class="<?=$alert?>">
+                                <?php
+                                if(isset($error['username'])) echo $error['username'];
+                                elseif(isset($error['password'])) echo $error['password'];
+                                ?>
+                            </div>
+                        <?php endif; ?>
                         
-                        <div class="<?=$alert?>">
-                            <?php
-                           if(isset($error['username'])){
-                            echo $error['username'];
-                        }
-                        elseif(isset($error['password'])){
-                            echo $error['password'];
-                        }
-                           ?>
+                        <div class="form-group">
+                            <label for="username">Username</label>
+                            <input type="text" id="username" name="uname" class="form-control" 
+                                   autocomplete="on" placeholder="Enter your username">
                         </div>
-                        <div class="form-group my-4">
-                            <label for="">Username</label>
-                            <input type="text" name="uname" class="form-control" autocomplete="on" placeholder="Enter your name">
+                        
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input type="password" id="password" name="password" class="form-control" 
+                                   autocomplete="on" placeholder="Enter your password">
                         </div>
-                        <div class="form-group my-4">
-                            <label for="">Password</label>
-                            <input type="password" name="password" class="form-control" autocomplete="on" placeholder="Enter your password">
-                         </div>
 
-                         <input type="submit" name="login" class="btn btn-success mx-auto d-block" value="Login">
+                        <button type="submit" name="login" class="btn btn-login">
+                            Login to Dashboard
+                        </button>
                     </form>
                 </div>
-                <div class="col-md-3"></div>
             </div>
         </div>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
