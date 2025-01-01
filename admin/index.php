7ade61382da8a6dc409acb25ebfde66acefef242
Ananthapadmanabhan M <ananthapadmanabhanr2@gmail.com>
2025-01-01 14:56:51 +0530

    Added some styles to admin dashboard

--- admin/index.php
+++ admin/index.php
@@ -7,9 +7,10 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Admin Dashboard - Healthcare Plus</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="./css/index.css">
 </head>
 <body>
     <?php
@@ -19,142 +20,144 @@ session_start();
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-2">
-                <?php
-                include("sidenav.php");
-                ?>
+                <?php include("sidenav.php"); ?>
             </div>
             <div class="col-md-10">
-                <h4 class="my-2">Admin Dashboard</h4>
+                <h4 class="dashboard-title">Admin Dashboard</h4>
                 <div class="row">
-                    <div class="col-md-3">
-                        <div class="card bg-success text-white m-1 mt-4">
+                    <!-- Admin Card -->
+                    <div class="col-md-3 mb-4">
+                        <div class="card admin-card text-white h-100">
                             <div class="card-body">
-                                <div class="row">
-                                    <div class="col-md-8">
-                                        <!-- Php admin query -->
+                                <div class="row align-items-center">
+                                    <div class="col-8">
                                         <?php
                                             $ad = mysqli_query($connect,"SELECT * FROM admin");
                                             $num = mysqli_num_rows($ad);
                                         ?>
-                                        <h5 class="my-2 fs-1"><?php echo $num; ?></h5>
-                                        <h5>Total</h5>
-                                        <h5>Admin</h5>
+                                        <h5 class="stat-number"><?php echo $num; ?></h5>
+                                        <p class="stat-label">Total Admin</p>
                                     </div>
-                                    <div class="col-md-4 text-center">
-                                        <a href="admin.php"><i class="fa-solid fa-hospital-user fa-2xl p-4" style="color: #ffffff;"></i></a>
+                                    <div class="col-4 icon-container">
+                                        <a href="admin.php">
+                                            <i class="fas fa-hospital-user"></i>
+                                        </a>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-3">
-                        <div class="card bg-info text-white m-1 mt-4">
+
+                    <!-- Doctor Card -->
+                    <div class="col-md-3 mb-4">
+                        <div class="card doctor-card text-white h-100">
                             <div class="card-body">
-                                <div class="row">
-                                    <div class="col-md-8">
+                                <div class="row align-items-center">
+                                    <div class="col-8">
                                         <?php
-                                        $doctor = mysqli_query($connect,"SELECT * FROM doctors WHERE status='Approved'");
-                                        $num2 = mysqli_num_rows($doctor);
+                                            $doctor = mysqli_query($connect,"SELECT * FROM doctors WHERE status='Approved'");
+                                            $num2 = mysqli_num_rows($doctor);
                                         ?>
-                                        <h5 class="my-2 fs-1"><?php echo $num2; ?></h5>
-                                        <h5>Total</h5>
-                                        <h5>Doctor</h5>
+                                        <h5 class="stat-number"><?php echo $num2; ?></h5>
+                                        <p class="stat-label">Total Doctors</p>
                                     </div>
-                                    <div class="col-md-4 text-center">
-                                        <a href="doctor.php"><i class="fa-solid fa-user-doctor fa-2xl p-4" style="color: #ffffff;"></i></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-3">
-                        <div class="card bg-warning text-white m-1 mt-4">
-                            <div class="card-body">
-                                <div class="row">
-                                    <div class="col-md-8">
-
-                                    <?php
-
-                                    $p = mysqli_query($connect,"SELECT * FROM patient");
-
-                                    $pp = mysqli_num_rows($p);
-
-                                    ?>
-                                        <h5 class="my-2 fs-1"><?php echo $pp ?></h5>
-                                        <h5>Total</h5>
-                                        <h5>Patient</h5>
-                                    </div>
-                                    <div class="col-md-4 text-center">
-                                        <a href="patient.php"><i class="fa-solid fa-bed fa-2xl p-4" style="color: #ffffff;"></i></a>
+                                    <div class="col-4 icon-container">
+                                        <a href="doctor.php">
+                                            <i class="fas fa-user-md"></i>
+                                        </a>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-3">
-                        <div class="card bg-danger text-white m-1 mt-4">
+
+                    <!-- Patient Card -->
+                    <div class="col-md-3 mb-4">
+                        <div class="card patient-card text-white h-100">
                             <div class="card-body">
-                                <div class="row">
-                                    <div class="col-md-8">
-
-                                    <?php
-                                    $re = mysqli_query($connect,"SELECT * FROM report");
-
-                                    $ree = mysqli_num_rows($re);
-
-                                    ?>
-                                        <h5 class="my-2 fs-1"><?php echo $ree ?></h5>
-                                        <h5>Total</h5>
-                                        <h5>Report</h5>
-                                    </div>
-                                    <div class="col-md-4 text-center">
-                                        <a href="report.php"><i class="fa-solid fa-flag fa-2xl p-4" style="color: #ffffff;"></i></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-3">
-                        <div class="card bg-warning text-white m-1 mt-4">
-                            <div class="card-body">
-                                <div class="row">
-                                    <div class="col-md-8">
-                                    <?php
-
-                                        $job = mysqli_query($connect, "SELECT * FROM doctors WHERE status='pending'");
-                                        $num1 = mysqli_num_rows ($job);
-
-                                    ?>
-                                        <h5 class="my-2 fs-1"><?php echo $num1; ?></h5>
-                                        <h5>Total</h5>
-                                        <h5>Signup Request</h5>
-                                    </div>
-                                    <div class="col-md-4 text-center">
-                                        <a href="job_request.php"><i class="fa-solid fa-book-open-reader fa-2xl p-4" style="color: #ffffff;"></i></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-3">
-                        <div class="card bg-success text-white m-1 mt-4">
-                            <div class="card-body">
-                                <div class="row">
-                                    <div class="col-md-8">
+                                <div class="row align-items-center">
+                                    <div class="col-8">
                                         <?php
-                                        $in = mysqli_query($connect,"SELECT sum(amount_paid) as profit from income");
-
-                                        $row = mysqli_fetch_array($in);
-
-                                        $inc = $row['profit'];
-                                        
+                                            $p = mysqli_query($connect,"SELECT * FROM patient");
+                                            $pp = mysqli_num_rows($p);
                                         ?>
-                                        <h5 class="my-2 fs-1"><?php echo $inc."Rs" ?></h5>
-                                        <h5>Total</h5>
-                                        <h5>Income</h5>
+                                        <h5 class="stat-number"><?php echo $pp; ?></h5>
+                                        <p class="stat-label">Total Patients</p>
                                     </div>
-                                    <div class="col-md-4 text-center">
-                                        <a href="income.php"><i class="fa-solid fa-indian-rupee-sign fa-2xl p-4" style="color: #ffffff;"></i></a>
+                                    <div class="col-4 icon-container">
+                                        <a href="patient.php">
+                                            <i class="fas fa-bed"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Report Card -->
+                    <div class="col-md-3 mb-4">
+                        <div class="card report-card text-white h-100">
+                            <div class="card-body">
+                                <div class="row align-items-center">
+                                    <div class="col-8">
+                                        <?php
+                                            $re = mysqli_query($connect,"SELECT * FROM report");
+                                            $ree = mysqli_num_rows($re);
+                                        ?>
+                                        <h5 class="stat-number"><?php echo $ree; ?></h5>
+                                        <p class="stat-label">Total Reports</p>
+                                    </div>
+                                    <div class="col-4 icon-container">
+                                        <a href="report.php">
+                                            <i class="fas fa-flag"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Job Request Card -->
+                    <div class="col-md-3 mb-4">
+                        <div class="card request-card text-white h-100">
+                            <div class="card-body">
+                                <div class="row align-items-center">
+                                    <div class="col-8">
+                                        <?php
+                                            $job = mysqli_query($connect, "SELECT * FROM doctors WHERE status='pending'");
+                                            $num1 = mysqli_num_rows($job);
+                                        ?>
+                                        <h5 class="stat-number"><?php echo $num1; ?></h5>
+                                        <p class="stat-label">Signup Requests</p>
+                                    </div>
+                                    <div class="col-4 icon-container">
+                                        <a href="job_request.php">
+                                            <i class="fas fa-book-open"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Income Card -->
+                    <div class="col-md-3 mb-4">
+                        <div class="card income-card text-white h-100">
+                            <div class="card-body">
+                                <div class="row align-items-center">
+                                    <div class="col-8">
+                                        <?php
+                                            $in = mysqli_query($connect,"SELECT sum(amount_paid) as profit from income");
+                                            $row = mysqli_fetch_array($in);
+                                            $inc = $row['profit'];
+                                        ?>
+                                        <h5 class="stat-number"><?php echo $inc; ?>₹</h5>
+                                        <p class="stat-label">Total Income</p>
+                                    </div>
+                                    <div class="col-4 icon-container">
+                                        <a href="income.php">
+                                            <i class="fas fa-rupee-sign"></i>
+                                        </a>
                                     </div>
                                 </div>
                             </div>
